05f6b00151470c8f191318f36e8f59cf7d4c37ea
nada alami chentoufi <89505314+MRX418@users.noreply.github.com>
2023-04-21 04:01:56 -0400

    partie 2 done

--- src/nuanceurFragments.glsl
+++ src/nuanceurFragments.glsl
@@ -5,6 +5,7 @@ uniform int texnumero;
 
 in Attribs {
     vec4 couleur;
+    vec2 texCoord;
 } AttribsIn;
 
 out vec4 FragColor;
@@ -14,7 +15,13 @@ void main( void )
     // Mettre un test bidon afin que l'optimisation du compilateur n'élimine l'attribut "couleur".
     // Vous ENLEVEREZ cet énoncé inutile!
     // if ( AttribsIn.couleur.r + texnumero + texture(leLutin,vec2(0.0)).r < 0.0 ) discard;
-
-    //FragColor = texture( leLutin, gl_PointCoord );
-    FragColor = AttribsIn.couleur;
+     if( texnumero != 0){ //lorsque lutin est utilise
+        //partie qui ne contient pas la texture devient transparente
+        vec4 texel = texture( leLutin, AttribsIn.texCoord );
+        if ( texel.a < 0.1 ) discard;
+        FragColor.rgb = mix( AttribsIn.couleur.rgb, texel.rgb, 0.6 );
+        FragColor.a = AttribsIn.couleur.a;
+    } else {
+        FragColor = AttribsIn.couleur;
+    }
 }
--- src/nuanceurGeometrie.glsl
+++ src/nuanceurGeometrie.glsl
@@ -25,11 +25,14 @@ in Attribs {
 
 out Attribs {
     vec4 couleur;
+    vec2 texCoord;
 } AttribsOut;
 
 // la hauteur minimale en-dessous de laquelle les lutins ne tournent plus (partie 3)
 const float hauteurInerte = 8.0;
-
+vec2 rotate(vec2 v, float angle) {
+	return mat2( cos(angle) , - sin(angle) ,  sin(angle) , cos(angle) ) * v;
+}
 void main()
 {
     vec2 coins[4];
@@ -41,18 +44,26 @@ void main()
  // à partir du point, créer quatre points qui servent de coin aux deux triangles
     for ( int i = 0 ; i < 4 ; ++i )
     {
+        vec2 decalage = coins[i]; // on positionne successivement aux quatre coins
+        AttribsOut.texCoord = coins[i] + vec2( 0.5, 0.5 ); // on utilise coins[] pour définir des coordonnées de texture
+        if(texnumero==1){
+            const float nlutins = 16.0; // 16 positions de vol dans la texture
+            int num = int ( mod ( 18.0 * AttribsIn[0].tempsDeVieRestant , nlutins ) ); // 18 Hz
+            AttribsOut.texCoord.s += num;
+            AttribsOut.texCoord.s /= nlutins ;
+        }
+        else if(texnumero==2){
+            decalage = rotate(coins[i], 6.0 * AttribsIn[0].tempsDeVieRestant);
+        }
         // assigner la position du point
         gl_Position = matrProj * gl_in[0].gl_Position;
-
         // assigner la taille des points (en pixels)
         gl_PointSize = gl_in[0].gl_PointSize;
 
-        vec2 decalage = coins[i]; // on positionne successivement aux quatre coins
         vec4 pos = vec4( gl_in[0].gl_Position.xy + gl_PointSize * decalage, gl_in[0].gl_Position.zw );
         gl_Position = matrProj * pos;    // on termine la transformation débutée dans le nuanceur de sommets
         // assigner la couleur courante
         AttribsOut.couleur = AttribsIn[0].couleur;
-        // AttribsOut.texCoord = coins[i] + vec2( 0.5, 0.5 ); // on utilise coins[] pour définir des coordonnées de texture
         EmitVertex();
     }
 }
